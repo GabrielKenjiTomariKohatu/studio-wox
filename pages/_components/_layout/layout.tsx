24445503d27ed127280db0edc73b8e139213375e
GabrielKenjiTomariKohatu <gabrielkohatu@gmail.com>
2024-08-25 20:06:33 -0300

    fix(layout.tsx): Ajuste responsividade Layout

--- pages/_components/_layout/layout.tsx
+++ pages/_components/_layout/layout.tsx
@@ -25,19 +25,19 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Footer>
                 <section className={`w-100 p-5 d-flex justify-content-center ${styles['layout-section-footer']}`}>
                     <div className="row container d-flex justify-content-center" style={{ gap: '60px 0' }}>
-                        <div className="col-sm-6 col-md-4 col-lg p-sm-0">
+                        <div className="col-sm-6 col-md-6 col-lg p-sm-0 d-flex flex-column align-items-center align-items-lg-start">
                             <Link href="/" aria-label="Studio Wox Logo 2" className={`d-block ${styles['cursor-pointer']}`}>
                                 <LogoStudioWox2 style={{ width: '100%', height: 'auto' }} />
                             </Link>
                         </div>
 
-                        <nav className="col-sm-6 col-md-4 col-lg d-flex flex-column">
-                            <div className="mb-4">
+                        <nav className="col-sm-6 col-md-6 col-lg d-flex flex-column align-items-center align-items-lg-start">
+                            <div className="mb-4 d-flex flex-column align-items-center align-items-lg-start">
                                 <h4 className={`text-white ${styles['min-height-itens']}`}>Principais tipos de imóveis</h4>
                                 <hr className={styles.line} />
                             </div>
 
-                            <ul className={`d-flex flex-column ${styles['gap-itens']} ${styles['no-list-style']}`}>
+                            <ul className={`d-flex flex-column align-items-center align-items-lg-start ${styles['gap-itens']} ${styles['no-list-style']}`}>
                                 <li>
                                     <a href="#" className={`text-light user-select-auto ${styles['cursor-pointer']}`}>
                                         Apartamentos prontos
@@ -70,13 +70,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                             </ul>
                         </nav>
 
-                        <nav className="col-sm-6 col-md-4 col-lg d-flex flex-column">
-                            <div className="mb-4">
+                        <nav className="col-sm-6 col-md-6 col-lg d-flex flex-column align-items-center align-items-lg-start">
+                            <div className="mb-4 d-flex flex-column align-items-center align-items-lg-start">
                                 <h4 className={`text-white ${styles['min-height-itens']}`}>Principais bairros</h4>
                                 <hr className={styles.line} />
                             </div>
 
-                            <ul className={`d-flex flex-column ${styles['gap-itens']} ${styles['no-list-style']}`}>
+                            <ul className={`d-flex flex-column align-items-center align-items-lg-start ${styles['gap-itens']} ${styles['no-list-style']}`}>
                                 <li>
                                     <a href="#" className={`text-light user-select-auto ${styles['cursor-pointer']}`}>
                                         Jardins
@@ -108,13 +108,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                             </ul>
                         </nav>
 
-                        <nav className="col-sm-6 col-md-4 col-lg d-flex flex-column">
-                            <div className="mb-4">
+                        <nav className="col-sm-6 col-md-6 col-lg d-flex flex-column align-items-center align-items-lg-start">
+                            <div className="mb-4 d-flex flex-column align-items-center align-items-lg-start">
                                 <h4 className={`text-white ${styles['min-height-itens']}`}>Imóveis por cidade</h4>
                                 <hr className={styles.line} />
                             </div>
 
-                            <ul className={`d-flex flex-column ${styles['gap-itens']} ${styles['no-list-style']}`}>
+                            <ul className={`d-flex flex-column align-items-center align-items-lg-start ${styles['gap-itens']} ${styles['no-list-style']}`}>
                                 <li>
                                     <a href="#" className={`text-light user-select-auto ${styles['cursor-pointer']}`}>
                                         São Paulo
@@ -151,28 +151,28 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
                 <section className={`d-flex justify-content-center align-items-center ${styles['min-heigh-footer']}`}>
                     <div className="row container d-flex align-items-center justify-content-center py-3 p-lg-0" style={{ gap: '20px 0px' }}>
-                        <article className={`col-md-4 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
+                        <article className={`col-md-5 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
                             <MoneyBook />
                             <p className="m-0 pl-2 font-weight-bold">Parcelas fixas durante a obra</p>
                         </article>
 
                         <hr className={`${styles['line-vertical']} d-none d-md-block`} />
 
-                        <article className={`col-md-4 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
+                        <article className={`col-md-5 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
                             <RealTimeBidding />
                             <p className="m-0 pl-2 font-weight-bold">Compra 100% digital</p>
                         </article>
 
-                        <hr className={`${styles['line-vertical']} d-none d-md-block`} />
+                        <hr className={`${styles['line-vertical']} d-none d-lg-block`} />
 
-                        <article className={`col-md-4 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
+                        <article className={`col-md-5 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
                             <Timer />
                             <p className="m-0 pl-2 font-weight-bold">Fast dating</p>
                         </article>
 
-                        <hr className={`${styles['line-vertical']} d-none d-lg-block`} />
+                        <hr className={`${styles['line-vertical']} d-none d-md-block`} />
 
-                        <article className={`col-md-4 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
+                        <article className={`col-md-5 col-sm-6 col-lg text-white d-flex justify-content-center align-items-center ${styles['custom-height-56']}`}>
                             <Ribbon />
                             <p className="m-0 pl-2 font-weight-bold">Prêmios</p>
                         </article>
